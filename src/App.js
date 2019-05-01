9368d2d59b1c5bd1271f8fb0c08e4021e27004ff
nazar-kushniruk <kushniruknazar@gmail.com>
2019-05-01 20:16:50 +0300

    work with test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,69 @@
-import React from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import FormForTests from './components/FormForTests';
+// import React from 'react';
+// import './App.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import FormForTests from './components/FormForTests';
+//
+// function App() {
+//   return (
+//     <div className="container"
+//     style={{'marginTop':40}}>
+//       <FormForTests/>
+//     </div>
+//   );
+// }
+//
+// export default App;
 
-function App() {
-  return (
-    <div className="container"
-    style={{'marginTop':40}}>
-      <FormForTests/>
-    </div>
-  );
+
+import React, { Component } from 'react';
+
+export const doIncrement = (prevState) => ({ counter: prevState.counter + 1,});
+export const doDecrement = (prevState) => ({ counter: prevState.counter - 1,});
+
+class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            counter: 0,
+        };
+
+        this.onIncrement = this.onIncrement.bind(this);
+        this.onDecrement = this.onDecrement.bind(this);
+    }
+
+    onIncrement() {
+        this.setState(doIncrement);
+    }
+
+    onDecrement() {
+        this.setState(doDecrement);
+    }
+
+    render() {
+        const { counter } = this.state;
+
+        return (
+            <div>
+                <h1>My Counter</h1>
+                <Counter counter={counter} />
+
+                <button
+                    type="button"
+                    onClick={this.onIncrement}
+                >
+                    Increment
+                </button>
+
+                <button
+                    type="button"
+                    onClick={this.onDecrement}
+                >
+                    Decrement
+                </button>
+            </div>
+        );
+    }
 }
-
+export const Counter = ({counter}) => <p>{counter}</p>;
 export default App;
--- src/components/FormForTests.js
+++ src/components/FormForTests.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 
 class FormForTests extends Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
         this.state = {
             login: '',
             password: '',
